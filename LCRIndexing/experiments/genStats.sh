1d053f634a2df6b7552b89e2a8267146f3a5fd5a
pickerxxr <18307100052@fudan.edu.cn>
2022-01-17 10:49:11 +0800

    delete

--- LCRIndexing/experiments/genStats.sh
+++
@@ -1,9 +0,0 @@
-datasets=`find experiments/graphs/ | egrep '\.edge$' | sed 's:\.edge::g'`;
-for ds in $datasets;
-do
-    if [ ! -f "${ds}.csv" ];
-    then
-        echo "build/default/genStats ${ds}"
-        build/default/genStats $ds
-    fi
-done;
--- LCRIndexing/rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf build/ ./waf-* .lock-script
-./waf configure
-./waf build
